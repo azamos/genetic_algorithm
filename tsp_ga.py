ba6c3c37b80c52b1d3b35b25268920a1e2387b0f
amos zohar <azamoszohar@gmail.com>
2024-01-23 14:05:24 +0200

    procuded a superior result to the random algo

--- tsp_ga.py
+++ tsp_ga.py
@@ -1,6 +1,7 @@
 import math,random,sys
 
-MUTATION_BARRIER = 20
+MUTATION_BARRIER = 30
+MAX_GENERATIONS = 1000
 
 def solve(points):
     n = len(points)
@@ -11,29 +12,32 @@ def solve(points):
 
 def evolution(population):
     n = len(population)
-    if n==1:
-        return population[0]
-    #sort the population by fitness
-    graded_population = sorted(population,key=fitness)
-    #will take only the top half candidates for mating 
-    viable_candidates = graded_population[n//2:n]
-    next_generation = []
-    for i in range(1,n//2,2):
-        cross_point = random.randint(0,n-1)
-        parent1 = viable_candidates[i-1]
-        child = parent1[0:cross_point]
-        parent2 = viable_candidates[i]
-        remaining_parent2 = [p for p in parent2 if p not in child]
-        child += remaining_parent2
-        dice_roll = random.randint(1,100)
-        if dice_roll <= MUTATION_BARRIER:
-            #If a mutation occured, pick a random index,
-            #and replace it with n-1-index
-            mutatedIndex = random.randint(0,n-1)
-            #cooler way to perform swap
-            child[mutatedIndex],child[n-1-mutatedIndex] = child[n-1-mutatedIndex],child[mutatedIndex]
-        next_generation.append(child)
-    return evolution(next_generation)
+    for gen_iter in range(0,MAX_GENERATIONS):
+        #sort the population by fitness
+        graded_population = sorted(population,key=fitness)
+        #will take only the top 66% candidates for mating 
+        viable_candidates = graded_population[0:2*n//3]
+        next_generation = []
+        for i in range(1,n//2,2):
+            cross_point = random.randint(0,n-1)
+            parent1 = viable_candidates[i-1]
+            child = parent1[0:cross_point]
+            parent2 = viable_candidates[i]
+            remaining_parent2 = [p for p in parent2 if p not in child]
+            child += remaining_parent2
+            #check if mutated
+            dice_roll = random.randint(1,100)
+            if dice_roll <= MUTATION_BARRIER:
+                #If a mutation occured, pick a random index,
+                #and replace it with n-1-index
+                mutatedIndex = random.randint(0,n-1)
+                #cooler way to perform swap
+                child[mutatedIndex],child[n-1-mutatedIndex] = child[n-1-mutatedIndex],child[mutatedIndex]
+            next_generation.append(child)
+        #since the crossings produces 0.33n children, I take the top 67% of the original generation
+        #to the next one, discarding only the 33% worst original candidates
+        population = viable_candidates + next_generation
+    return sorted(population,key = fitness)[0]
 
 def dist(p1,p2):
     return float(math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2))
